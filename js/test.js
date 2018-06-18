be44c2f68f539defb11af201d949886d54fab77c
Miss Raoudha <raoudha2017@outlook.fr>
2018-06-18 13:03:06 +0100

    this is the web site

--- js/test.js
+++ js/test.js
@@ -1,6 +1,12 @@
-$(document).ready(function (){
-     /* $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');*/
-    	$(window).scroll(function () {
+$(document).ready(function ()
+{
+    
+     $('.carousel').carousel(
+   {    interval:10000,
+        pause: false
+   });
+    
+       $(window).scroll(function () {
 			if ($(this).scrollTop() != 0) {
 				$('#toTop').fadeIn();
 			} else {
@@ -14,44 +20,6 @@ $(document).ready(function (){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
-   
-
-//show demo wrapper when click on the fa-fw
-
-$('.sett').click(function () {
-                $("#demo-wrapper").toggle("slide");
-});
-
-//end
-
-
- /* switcher Colors   
-$(".color1" ).click(function(){
-   
-		$("#demo-wrapper ul li" ).attr("href", "css/Default-theme.css");
-		return false;
-	});
-
-
-	$(".color2" ).click(function(){
-		$("#demo-wrapper ul li" ).attr("href", "css/gree-theme.css ");
-		return false;
-        
-	});
-
-	$(".color3" ).click(function(){
-		$("#demo-wrapper" ).attr("href", "css/pink-theme.css" );
-		return false;
-	});
-
-	$(".color4" ).click(function(){
-		$("#demo-wrapper" ).attr("href", "css/blue-theme.css" );
-		return false;
-	});
-
-  */  
-    
- 
 
  // jQuery for page scrolling feature - requires jQuery Easing plugin
  $(document).on('click', ' a.page-scroll ', 
@@ -64,11 +32,6 @@ $(".color1" ).click(function(){
 	
 	event.preventDefault();
     });
+//End
 
-
-    $('.carousel').carousel(
-   {    interval: 3000,
-  pause: false
    });
-
-});
